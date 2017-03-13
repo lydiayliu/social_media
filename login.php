e93eebdd32f54d336325ef6f5d8afb05af5381e3
foreverwander <lydia.liu@mail.utoronto.ca>
2017-03-13 12:19:02 +0000

    update look of login and register pages

--- login.php
+++ login.php
@@ -1,53 +1,65 @@
 <?php
-  include("dbconfig.php");
-  session_start();
+include("dbconfig.php");
+session_start();
 
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-    foreach($_POST as $key=>$value) {
-      if(empty($_POST[$key])) {
-        $message = ucwords($key) . " field is required";
-        break;
-      }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($_POST as $key => $value) {
+        if (empty($_POST[$key])) {
+            $message = ucwords($key) . " field is required";
+            break;
+        }
     }
-    if (!isset($message) ){
-      $user_email = mysqli_real_escape_string($conn, $_POST['email']);
-      $password = mysqli_real_escape_string($conn, $_POST['password']);
+    if (!isset($message)) {
+        $user_email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-      $sql = "SELECT * FROM account WHERE email_address = '$user_email'";
-      $result = mysqli_query($conn, $sql);
-      $count = mysqli_num_rows($result);
-      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      $hashed_password = $row['password'];
+        $sql = "SELECT * FROM account WHERE email_address = '$user_email'";
+        $result = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($result);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $hashed_password = $row['password'];
 
-      // If result matched $myusername and $mypassword, table row must be 1 row
+        // If result matched $myusername and $mypassword, table row must be 1 row
 
-      if( password_verify($password, $hashed_password) && $count == 1) {
-        $_SESSION['login_user'] = $user_email;
-        if ($row['isAdmin'] == 1)
-          header("location:admin_welcome.php");
-        else
-          header("location: welcome.php");
-      } else {
-        $message = "Your Login Name or Password is invalid";
-      }
+        if (password_verify($password, $hashed_password) && $count == 1) {
+            $_SESSION['login_user'] = $user_email;
+            if ($row['isAdmin'] == 1)
+                header("location:admin_welcome.php");
+            else
+                header("location: welcome.php");
+        } else {
+            $message = "Your Login Name or Password is invalid";
+        }
     }
-  }
-  $conn->close();
+}
+$conn->close();
 ?>
 
 <html>
-  <head>
-    <title>Login Page</title>
-  </head>
-  <div ><b>Login</b></div>
-  <form action = "" method = "post">
-    <label>Email :</label><input type="text" name="email" class="box"><br/>
-    <label>Password :</label><input type="password" name="password" class="box"><br/>
-    <input type = "submit" value = " Submit "/><br />
-  </form>
-  <form action="register.php" target="_blank" action="">
-    <button>Register</button>
-  </form>
-  <div><?php if(isset($message)) echo $message; ?></div>
-  </body>
+    <head>
+        <?php require_once('head.php'); ?>
+        <title>Login Page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Assignment Book</h1>
+                <p>welcome to assignment book!</p>
+            </div>
+        </div>
+        <div class="container">
+            <div ><b>Login</b></div>
+            <form action = "" method = "post">
+                <label>Email :</label><input type="text" name="email" class="box"><br/>
+                <label>Password :</label><input type="password" name="password" class="box"><br/>
+                <input class="btn btn-primary" type = "submit" value = " Submit "/><br />
+            </form>
+            <form action="register.php" target="_blank" action="">
+                <button class="btn btn-primary">Register</button>
+            </form>
+            <div><?php if (isset($message)) echo $message; ?></div>
+
+        </div>
+        <?php require_once('common_footer.html'); ?>
+    </body>
 </html>
--- register.php
+++ register.php
@@ -1,8 +1,7 @@
 <?php
-
 include("dbconfig.php");
 
-function new_account($dbconn){
+function new_account($dbconn) {
 
     $userpassword = mysqli_real_escape_string($dbconn, $_POST["password"]);
     $hashedpassword = password_hash($userpassword, PASSWORD_DEFAULT);
@@ -22,102 +21,111 @@ function new_account($dbconn){
     } else {
         echo "Error: " . "<br>" . $dbconn->error;
     }
-
-
 }
-  /* Form Required Field Validation */
+
+/* Form Required Field Validation */
 
 
 if (count($_POST) > 0) {
-  foreach($_POST as $key=>$value) {
-    if(empty($_POST[$key])) {
-      $message = ucwords($key) . " field is required";
-      break;
+    foreach ($_POST as $key => $value) {
+        if (empty($_POST[$key])) {
+            $message = ucwords($key) . " field is required";
+            break;
+        }
     }
-  }
-  /* Password Matching Validation */
-  if($_POST["password"] != $_POST["confirm_password"]){
-    $message = 'Passwords should be same<br>';
-  }
+    /* Password Matching Validation */
+    if ($_POST["password"] != $_POST["confirm_password"]) {
+        $message = 'Passwords should be same<br>';
+    }
 
-  /* Email Validation */
-  if(!isset($message)) {
-    if (!filter_var($_POST["email_address"], FILTER_VALIDATE_EMAIL)) {
-      $message = "Invalid UserEmail";
+    /* Email Validation */
+    if (!isset($message)) {
+        if (!filter_var($_POST["email_address"], FILTER_VALIDATE_EMAIL)) {
+            $message = "Invalid UserEmail";
+        }
     }
-  }
 
-  /* Validation to check if Terms and Conditions are accepted */
-  if(!isset($message)) {
-    if(!isset($_POST["terms"])) {
-      $message = "Accept Terms and conditions before submit";
+    /* Validation to check if Terms and Conditions are accepted */
+    if (!isset($message)) {
+        if (!isset($_POST["terms"])) {
+            $message = "Accept Terms and conditions before submit";
+        }
     }
-  }
-  if (!isset($message)){
-    $new_account_email = mysqli_real_escape_string($conn, $_POST["email_address"]);
-    $exist_user_query = "SELECT email_address FROM account WHERE email_address = '$new_account_email'";
-    $result = $conn->query($exist_user_query);
-    if ($result->num_rows > 0) {
-        $message = "Email already exist";
+    if (!isset($message)) {
+        $new_account_email = mysqli_real_escape_string($conn, $_POST["email_address"]);
+        $exist_user_query = "SELECT email_address FROM account WHERE email_address = '$new_account_email'";
+        $result = $conn->query($exist_user_query);
+        if ($result->num_rows > 0) {
+            $message = "Email already exist";
+        } else {
+            new_account($conn);
+            header("location: index.php");
+            $conn->close();
+        }
     }
-    else {
-      new_account($conn);
-      header("location: index.php");
-      $conn->close();
-    }
-  }
 }
-
 ?>
 <html>
-  <body>
+    <head>
+        <?php require_once('head.php'); ?>
+        <title>Registration Page</title>
+    </head>  
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Assignment Book</h1>
+                <p>welcome to assignment book!</p>
+            </div>
+        </div>
+        <div class="container">
+            <div ><b>Register</b></div>
+            <form name="frmRegistration" method="post" action="">
+                <table border="0" width="500" class="demo-table">
 
-    <form name="frmRegistration" method="post" action="">
-    	<table border="0" width="500" class="demo-table">
+                    <div><?php if (isset($message)) echo $message; ?></div>
 
-        <div><?php if(isset($message)) echo $message; ?></div>
-
-    		<tr><td>Email Address</td>
-    		<td><input type="text" class="demoInputBox" name="email_address" value="<?php if(isset($_POST['email_address'])) echo $_POST['email_address']; ?>"></td>
-    		</tr>
-    		<tr><td>Name</td>
-    		<td><input type="text" class="demoInputBox" name="name" value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>"></td>
-    		</tr>
-    		<tr><td>Password</td>
-    		<td><input type="password" class="demoInputBox" name="password" value=""></td>
-    		</tr>
-    		<tr><td>Confirm Password</td>
-    		<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-    		</tr>
-        <tr><td>Age</td>
-    		<td><input type="number" class="demoInputBox" name="age" value="<?php if(isset($_POST['age'])) echo $_POST['age']; ?>"></td>
-    		</tr>
-        <tr><td>Country</td>
-        <td><input type="text" class="demoInputBox"  name="country" value="<?php if(isset($_POST['country'])) echo $_POST['country']; ?>"></td>
-        </tr>
-        <tr><td>City</td>
-        <td><input type="text" class="demoInputBox"  name="city" value="<?php if(isset($_POST['city'])) echo $_POST['city']; ?>"></td>
-        </tr>
-        <tr><td>Introduction</td>
-        <td><textarea name="introduction" rows="5" cols="50" value="<?php if(isset($_POST['introduction'])) echo $_POST['introduction']; ?>"><?php if(isset($_POST['introduction'])) echo $_POST['introduction']; ?></textarea></td>
-        </tr>
-        <tr><td>Privacy Setting</td>
-        <td><select name="privacy">
-              <option value="public">Public</option>
-              <option value="friends_only">Friends Only</option>
-              <option value="private">Private</option>
-            </select>
-    		</td>
-        </tr>
-        <tr>
-    		<td><input type="checkbox" name="terms"> I accept Terms and Conditions</td>
-    		</tr>
-    	</table>
-  	<div><input type="submit" name="submit" value="Register" class="btn btn-primary"></div>
-  </form>
-  <form action="index.php">
-    <input type="submit" value="Index" />
-  </form>
-
-  </body>
+                    <tr><td>Email Address</td>
+                        <td><input type="text" class="demoInputBox" name="email_address" value="<?php if (isset($_POST['email_address'])) echo $_POST['email_address']; ?>"></td>
+                    </tr>
+                    <tr><td>Name</td>
+                        <td><input type="text" class="demoInputBox" name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>"></td>
+                    </tr>
+                    <tr><td>Password</td>
+                        <td><input type="password" class="demoInputBox" name="password" value=""></td>
+                    </tr>
+                    <tr><td>Confirm Password</td>
+                        <td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
+                    </tr>
+                    <tr><td>Age</td>
+                        <td><input type="number" class="demoInputBox" name="age" value="<?php if (isset($_POST['age'])) echo $_POST['age']; ?>"></td>
+                    </tr>
+                    <tr><td>Country</td>
+                        <td><input type="text" class="demoInputBox"  name="country" value="<?php if (isset($_POST['country'])) echo $_POST['country']; ?>"></td>
+                    </tr>
+                    <tr><td>City</td>
+                        <td><input type="text" class="demoInputBox"  name="city" value="<?php if (isset($_POST['city'])) echo $_POST['city']; ?>"></td>
+                    </tr>
+                    <tr><td>Introduction</td>
+                        <td><textarea name="introduction" rows="5" cols="50" value="<?php if (isset($_POST['introduction'])) echo $_POST['introduction']; ?>"><?php if (isset($_POST['introduction'])) echo $_POST['introduction']; ?></textarea></td>
+                    </tr>
+                    <tr><td>Privacy Setting</td>
+                        <td><select name="privacy">
+                                <option value="public">Public</option>
+                                <option value="friends_only">Friends Only</option>
+                                <option value="private">Private</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="terms"> I accept Terms and Conditions</td>
+                    </tr>
+                </table>
+                <div><input type="submit" name="submit" value="Register" class="btn btn-primary"></div>
+            </form>
+            <form action="index.php">
+                <input class="btn btn-primary" type="submit" value="Index" />
+            </form>
+        </div>
+        <?php require_once('common_footer.html'); ?>
+    </body>
 </html>
